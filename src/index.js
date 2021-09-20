22df300b14da2e0ed53623f8aa08eccba902437b
이상철 <zlrz6706@gmail.com>
2021-09-20 22:47:00 +0900

    리액트 기초연습 초기설정화면

--- src/index.js
+++ src/index.js
@@ -4,14 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( // 특정 아이디를 가진 돔을 가져와서 거기에다가 리액트 컴포넌트를 그리겠다.
+    <App />,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
