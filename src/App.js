3663b5e3fe66fc0948c9e3cd327592fd797c6558
이상철 <zlrz6706@gmail.com>
2021-09-21 12:34:53 +0900

    state에 불변성을 유지해야 하는 이유와 이름으로 전화번호 찾기 구현

--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import PhoneInfoList from "./Component/PhoneInfoList";
 
 class App extends Component {
 
-  id = 0; //우리기 setState메소드를 사용하는 이유는 어떤 값이 수정됐을 때 Rerendering을 꼭 하게끔 하기 위함인데 id 값은 화면에 나올 필요가 없어서
+  id = 3; //우리기 setState메소드를 사용하는 이유는 어떤 값이 수정됐을 때 Rerendering을 꼭 하게끔 하기 위함인데 id 값은 화면에 나올 필요가 없어서
           // state안에 굳이 포함 시키지 않고 밖으로 뺐다.
 
   state= {
-    information : [],
+    information : [
+      {
+        id:0,
+        name: "홍길동",
+        phone: "010-0000-0001"
+      },
+      {
+        id:1,
+        name: "이상철",
+        phone: "010-0000-0002"
+      },
+      {
+        id:2,
+        name: "이안리",
+        phone: "010-0000-0003"
+      },
+    ],
+    keyword: "",
+  }
+   
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    })
   }
 
-  handleCreate = (data) => {
+
+   handleCreate = (data) => {
     const { information } = this.state; //비구조 할당문법
     this.setState({
       information: information.concat({
@@ -51,8 +75,15 @@ class App extends Component {
     return (
       <div>
           <PhoneForm onCreate={this.handleCreate} />
+          <input 
+            value={this.state.keyword}
+            onChange={this.handleChange}
+            placeholder="검색..." 
+          />
           <PhoneInfoList 
-          data={ this.state.information }
+          data={ this.state.information.filter(
+            info => info.name.indexOf(this.state.keyword) > -1
+          )}
           onRemove={this.handleRemove}
           onUpdate={this.handleUpdate}
           />
--- src/Component/PhoneInfo.js
+++ src/Component/PhoneInfo.js
@@ -7,6 +7,13 @@ class PhoneInfo extends Component {
     name: '',
     phone: '',
   }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state !== nextState) {
+      return true;
+    }
+    return this.props.info !== nextProps.info
+  }
+  
 
   handleRemove = () => {
     const { info, onRemove } = this.props;
@@ -51,6 +58,7 @@ class PhoneInfo extends Component {
       margin: "8px",
     }
 
+    console.log(name);
     return (
       <div style={ style }>
         {
